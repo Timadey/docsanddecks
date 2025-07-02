43b2bda71336cfb68354ca1f87c52311b8d036de
Timothy Adeleke <dlktimothy@gmail.com>
2025-07-02 12:20:13 +0100

    add https on in provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,6 +22,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(UrlGenerator $url): void
     {
         if (env('APP_ENV') == 'production') {
+            $this->app['request']->server->set('HTTPS', 'on');
             $url->forceScheme('https');
         }
         LogViewer::auth(function ($request) {
